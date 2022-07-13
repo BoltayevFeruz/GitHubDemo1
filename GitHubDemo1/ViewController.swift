fffe20b7400c8f3fc60cd4d24a15a3b7b46965a4
BoltayevFeruz <boltayevferuz777@gmail.com>
2022-07-13 23:44:57 +0500

    Initial Commit

---
+++ GitHubDemo1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubDemo1
+//
+//  Created by Feruz Boltayev on 13.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
